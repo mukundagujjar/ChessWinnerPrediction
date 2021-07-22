0ec2b599264744c5281e6b5b292815cf9e7d9f25
mukundagujjar <68166996+mukundagujjar@users.noreply.github.com>
2021-07-22 09:03:43 +0530

    Update chess.py

--- chess.py
+++ chess.py
@@ -2,7 +2,7 @@ import pickle
 import joblib
 import numpy as np
 import pandas as pd
-import tensorflow as tf
+import tensorflow-cpu as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
